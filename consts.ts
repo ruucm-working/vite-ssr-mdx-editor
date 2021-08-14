20d14d2a6c3236851bf1505ababbd9eedfc3f556
ruucm <ruucm.a@gmail.com>
2021-08-14 15:38:42 +0900

    fix2

--- consts.ts
+++ consts.ts
@@ -1,11 +1,11 @@
 // console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 
-// export const hostname =
-//   process.env.NODE_ENV === "development"
-//     ? "localhost"
-//     : "ec2-54-67-51-168.us-west-1.compute.amazonaws.com";
+export const hostname =
+  process.env.NODE_ENV === "development"
+    ? "localhost"
+    : "ec2-54-67-51-168.us-west-1.compute.amazonaws.com";
 
-export const hostname = "ec2-54-67-51-168.us-west-1.compute.amazonaws.com";
+// export const hostname = "ec2-54-67-51-168.us-west-1.compute.amazonaws.com";
 
 console.log("hostname", hostname);
 export const SOCKET_PORT = 33243; // better to use await getPort() for security
